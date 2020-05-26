d2c862f72fdaa2b31c0d9b7ed7c795873670849d
Axel Ballarin <axelbalmir@gmail.com>
2020-05-26 15:23:06 +0200

    add a simple seed and iframe column to lessons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_151842) do
+ActiveRecord::Schema.define(version: 2020_05_26_130817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_151842) do
     t.bigint "coach_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "iframe"
     t.index ["coach_id"], name: "index_lessons_on_coach_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Lesson.destroy_all
+Coach.destroy_all
+User.destroy_all
+Specialty.destroy_all
+
+
+puts "Creating database..."
+body_pump = Specialty.create!({ name: 'Body Pump' })
+
+axel = User.create!({ email: 'axel@gmail.com', password: "axel1234" })
+
+remi = Coach.create!({ name: 'Rémi', specialty: body_pump })
+
+friday_lesson = Lesson.create!({ name: 'lesson1', appointment: DateTime.now.to_s(:db), coach: remi })
+
+puts "Finished !"
