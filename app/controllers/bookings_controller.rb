ff5bc734df6314004b4b435883719aa4005c651f
Marie Belile <belilemarie@gmail.com>
2020-05-28 09:41:14 +0200

    modif des routes et ajout de create dans booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    # @booking = Booking.new(booking_params)
+    @booking = Booking.new(user_id: params[:user_id], lesson_id: params[:lesson_id])
+    @booking.save
+    redirect_to user_path(params[:user_id]), notice: 'This lesson has been successfully reserved.'
   end
 
   def update
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :lesson_id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/update'
-  get 'lessons', to: "lessons#index"
-  get "lessons/:id", to: "lessons#show", as: :lesson
-  get 'coaches/index'
   devise_for :users
+  resources :users do
+    resources :bookings
+  end
+  resources :coaches
+  resources :lessons
+  resources :specialties
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 end
