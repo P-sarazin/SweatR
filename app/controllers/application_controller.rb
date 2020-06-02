bef39f82b9f3804735cb3c4627f88e40cb69a065
Axel Ballarin <axelbalmir@gmail.com>
2020-06-02 10:29:14 +0200

    correct domain name in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   def default_url_options
-    { host: ENV["heroku config:set DOMAIN=www.sweatr.xyz/"] || "localhost:3000" }
+    { host: ENV["www.sweatr.xyz"] || "localhost:3000" }
   end
 end
