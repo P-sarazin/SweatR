f3413841f2d90f2f4d3e220bfb53c5946fab6499
Axel Ballarin <axelbalmir@gmail.com>
2020-06-04 16:05:34 +0200

    fix bug on dashboard lesson order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def dashboard
     @bookings = current_user.bookings
-    @next_bookings = @bookings.select{|booking| booking.lesson.appointment >= Time.now}.sort_by { |booking| booking.lesson.appointment }
+    @next_bookings = @bookings.select{|booking| booking.lesson.appointment >= Time.now}.sort_by { |booking| booking.lesson.appointment }.reverse
     @previous_bookings = @bookings.select{|booking| booking.lesson.appointment < Time.now}.sort_by { |booking| booking.lesson.appointment }
   end
 end
