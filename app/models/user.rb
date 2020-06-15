5c11e102afc6896808d2d6bd5759e856ae25e551
Axel Ballarin <axelbalmir@gmail.com>
2020-06-15 15:02:48 +0200

    added default avatar picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :set_default_avatar
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
+
+  def set_default_avatar
+    self.avatar_picture = image_tag("default_avatar.png")
+  end
 end
